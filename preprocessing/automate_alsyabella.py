9e9ed29f93cdd2b121d13c86812fab33022b958f
abel1494 <118557596+abel1494@users.noreply.github.com>
2025-12-23 18:32:01 +0700

    Update automate_alsyabella.py

--- preprocessing/automate_alsyabella.py
+++ preprocessing/automate_alsyabella.py
@@ -5,8 +5,6 @@ import warnings
 import shutil
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from bs4 import BeautifulSoup
 from bs4 import XMLParsedAsHTMLWarning
 import nltk
